26ebff73381b92a9239586f5cc0474883b175d90
George Tang <georgecctang@gmail.com>
2020-09-18 20:31:58 +0000

    created sumItems function

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+const sumItems = function (array) {
+  
+  total = 0;
+  
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      console.log('item is an array');
+      console.log('item is:', item);
+      total += sumItems(item);
+  } else {
+      console.log('item is not an array');
+      console.log('item is:', item);
+      total += item;
+    } 
+  }
+  return total;
 }
 
 module.exports = sumItems;
